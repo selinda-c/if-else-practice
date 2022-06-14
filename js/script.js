1c73aa40ac16150ffc40e3c1589fd3457fb804a9
Selinda-C <selindachapman@icloud.com>
2022-06-14 11:10:16 -0700

    add window onload function hiding all divs

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+  let cats = document.getElementById("cats");
+  cats.style.display = "none";
+  let dogs = document.getElementById("dogs");
+  dogs.style.display = "none";
+  let rabbits = document.getElementById("rabbits");
+  rabbits.style.display = "none";
+}
