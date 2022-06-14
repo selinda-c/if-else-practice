a860f574248302b7dcbefd7528ddd4275f0861de
Selinda-C <selindachapman@icloud.com>
2022-06-14 11:19:26 -0700

    add if / else statements

--- js/script.js
+++ js/script.js
@@ -5,4 +5,28 @@ window.onload = function(){
   dogs.style.display = "none";
   let rabbits = document.getElementById("rabbits");
   rabbits.style.display = "none";
+  let please = document.getElementById("notOnList")
+  please.style.display = "none"
+
+  const form = document.querySelector("form");
+
+  form.onsubmit = function(event){
+    const animals = document.querySelector("input#animal");
+    event.preventDefault();
+
+    if (animals === cats){
+      cats.removeAttribute("style")
+    }
+    else if (animals === dogs) {
+      dogs.removeAttribute("style")
+    }
+    else if (animals === rabbits){
+      rabbits.removeAttribute("style");
+    }
+    else {
+      
+    }
+
+  }
+
 }
