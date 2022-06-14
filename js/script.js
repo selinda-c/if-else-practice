d86a70bde3722b706ca3a4a409e037b787ebdb6c
Selinda-C <selindachapman@icloud.com>
2022-06-14 11:51:49 -0700

    FIX issue with divs not displaying on submit

--- js/script.js
+++ js/script.js
@@ -10,18 +10,27 @@ window.onload = function(){
 
   const form = document.querySelector("form");
   form.onsubmit = function(event){
-    const animals = document.querySelector("input#animal");
-    if (animals === cats){
-      cats.removeAttribute("style")
+    const animals = document.querySelector("input#animal").value;
+    if (animals === "cats"){
+      cats.removeAttribute("style");
+      dogs.style.display ="none";
+      rabbits.style.display = "none";
     }
-    else if (animals === dogs) {
+    else if (animals === "dogs") {
       dogs.removeAttribute("style")
+      rabbits.style.display = "none"
+      cats.style.display = "none"
     }
-    else if (animals === rabbits){
+    else if (animals === "rabbits"){
       rabbits.removeAttribute("style");
+      dogs.style.display = "none"
+      cats.style.display = "none"
     }
     else {
       please.removeAttribute("style")
+      dogs.style.display = "none"
+      cats.style.display = "none"
+      rabbits.style.display = "none"
     };
     event.preventDefault();
   };
